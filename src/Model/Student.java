cb3c5a06cf0633f7913e2e2f16ab9a38ba0acfd2
iamajaysingh <iamajaysingh4@gmail.com>
2020-07-21 17:06:06 +0530

    add functionality to enter multiple student in run class

--- src/Model/Student.java
+++ src/Model/Student.java
@@ -88,8 +88,6 @@ public class Student {
 				}
 				
 				coursesJoined.add(courseObj);
-				
-				
 				 
 			 }
 			 
--- src/Run.java
+++ src/Run.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import Model.Student;
 
 public class Run {
@@ -5,9 +7,16 @@ public class Run {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Student s = new Student();
-		 System.out.println("Student details" + s);
-		 
+		System.out.println("How many student you want to add. please enter here.");
+		Scanner s = new Scanner(System.in);
+	         int  studentCount =  s.nextInt();
+	         Student[] stuArray = new Student[studentCount];
+	         
+	         for(int i =0 ; i < stuArray.length;  i++) {
+	        	 stuArray[i]     =  new Student();
+	         }
+	         System.out.println("Please enter info of next student.");
+	         
 
 	}
 
